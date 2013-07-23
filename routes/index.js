8b8e35ad8bff748e08261009c088c1b604c3e62a
root <root@li164-128.members.linode.com>
2013-07-23 21:11:00 +0000

    pushing stash

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,11 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Wasatch Code' });
 };
+
+exports.about = function(req, res) {
+  res.render('about', {title: 'Who we are!'});
+};
+
+exports.contact = function(req, res) {
+  res.render('contact', {title: 'Contact Us!'});
+};
